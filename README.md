98fe13307be2d2ddffd00c52d3c4d8e8e720726c
Senecm <58762403+Senecm@users.noreply.github.com>
2021-10-20 14:50:16 +1030

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Writer
 The Writer Class Library for C# used throughout my projects
+
+## Explaination
+This is a simple resuable library containing a central for printing out text through methods at different intervals and with different console colors, as well as an implementation class which globalises the different forms of writers.
+
+### Build Instructions
+To add it to your project, simply clone the repository and add the project file to the dotnet reference with:
+```
+dotnet add {PROJECTDIRECTORY}/{PROJECTNAME.csproj} reference {REPOSITORYPATH}/Writer/Writer.csproj
+```
---
+++ Writer/Writer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace TextUtil
+{
+    public class Writer
+    {
+        public int Interval { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Writer()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Warning: No paramters detected! Behaving like normal WriteLine! Assuming intentional!");
+        }
+
+        public Writer(int paramInterval = 0, ConsoleColor paramColor = ConsoleColor.White)
+        {
+            this.Interval = paramInterval;
+            this.Color = paramColor;
+        }
+
+        // primary write function
+        public void Write(string input)
+        {
+            foreach (char c in input)
+            {
+                Console.ForegroundColor = Color;
+                Console.Write(c);
+                Thread.Sleep(Interval);   
+            }
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White; // double enforcement
+        }
+
+        public void WriteNL(string input)
+        {
+            foreach (char c in input)
+            {
+                Console.ForegroundColor = Color;
+                Console.Write(c);
+                Thread.Sleep(Interval);   
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White; // double enforcement
+        }
+    }
+
+    public static class Writers
+    {
+        public static Writer Writer = new Writer(100, ConsoleColor.White);
+        public static Writer Ok = new Writer(100, ConsoleColor.Green);
+        public static Writer Warn = new Writer(100, ConsoleColor.Yellow);
+        public static Writer Error = new Writer(100, ConsoleColor.Red);
+        public static Writer Sys = new Writer(0, ConsoleColor.Blue); 
+    }
+}
