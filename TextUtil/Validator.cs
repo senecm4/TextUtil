0f161c1d661e3080591d9cc528f285dce24a0597
Senecm <58762403+Senecm@users.noreply.github.com>
2021-10-20 20:44:53 +1030

    Change Repo: Make TextUtil library, refactor all files

---
+++ TextUtil/Validator.cs
@@ -0,0 +1,35 @@
+using System;
+using TextUtil;
+namespace TextUtil
+{
+    public class Validator
+    {
+        // public int UserInputInt { get; set; }
+        // public bool ValidMenuInput { get; set; }
+        public int validateINT(int userInputInt, bool validMenuInput)
+        {
+            try
+            {
+                userInputInt = Convert.ToInt32(Console.ReadLine());
+                validMenuInput = true;
+                return userInputInt;
+            }
+            catch (System.Exception) // may have to specifiy specific error in future
+            {
+                Writers.Error.WriteNL("Input Failure!");
+                userInputInt = -1;
+                return -1;
+            }
+        }
+
+        // public string validateSTRING(string userInputString, bool validMenuInput)
+        // {
+
+        // }
+    }
+
+    public static class Validators
+    {
+        public static Validator Validator = new Validator();
+    }
+}
